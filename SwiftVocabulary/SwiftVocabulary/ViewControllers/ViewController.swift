416414f571a891e2c9e4a1d3685eba4f117e912d
Seschwan <seschwanssd@Coreys-iMac.local>
2019-06-07 19:15:44 -0600

    Project Setup

---
+++ SwiftVocabulary/SwiftVocabulary/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Seschwan on 6/7/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
